9948fe96e071c6616830ea4f0a59fe5836bbf425
chrctr <hazovsvatoslav@gmail.com>
2026-09-09 22:01:42 +0300

    Added array tests

--- Lab1CPP/lab1.cpp
+++ Lab1CPP/lab1.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
@@ -21,43 +22,75 @@ Array *array_create_and_read(std::ifstream &input)
 
 void task1(Array *arr, std::ofstream &output)
 { 
+    size_t size = array_size(arr);
+    if (size == 0) {
+        output << std::endl;
+        std::cout << std::endl;
+        return;
+    }
+
     int max_ = array_get(arr, 0);
 
-    for (int i = 1; i < array_size(arr); i++)
+    for (size_t i = 1; i < size; i++)
     {
-        if (max_ < array_get(arr, i))
-        max_ = array_get(arr, i);
+        int val = array_get(arr, i);
+        if (max_ < val)
+            max_ = val;
     }
 
-    size_t in;
+    // Если максимальное значение меньше 2, то простых чисел нет
+    if (max_ < 2) {
+        for (size_t i = 0; i < size; i++) {
+            int n = array_get(arr, i);
+            if (n < 2) {
+                array_set(arr, i, 0);
+            }
+        }
+        // Вывод результата
+        for (size_t i = 0; i < size; i++) {
+            output << array_get(arr, i) << " ";
+            std::cout << array_get(arr, i) << " ";
+        }
+        output << std::endl;
+        std::cout << std::endl;
+        return;
+    }
 
-    bool* simple = new bool[max_];  
+    // ВАЖНО: выделяем max_ + 1, чтобы иметь доступ к индексу max_
+    bool* simple = new bool[max_ + 1];  
 
-    for (int i = 0; i < max_; i++) {
+    // Инициализация
+    for (int i = 0; i <= max_; i++) {
         simple[i] = true;
     }
-    simple[0] = simple[1] = false;
+    
+    // 0 и 1 - не простые числа
+    simple[0] = false;
+    if (max_ >= 1) simple[1] = false;
 
-    for (int i = 2; i * i < max_; i++) {
+    // Решето Эратосфена
+    for (int i = 2; i * i <= max_; i++) {
         if (simple[i]) {
-            for (int j = i * i; j < max_; j += i) {
+            for (int j = i * i; j <= max_; j += i) {
                 simple[j] = false;
             }
         }
     }
 
-    for (int i = 0; i < array_size(arr); i++) {
-        int n = array_get(arr, i); 
-        if (!simple[n]) {        
-            array_set(arr, i, 0);   
+    // Заменяем непростые числа на 0
+    for (size_t i = 0; i < size; i++) {
+        int n = array_get(arr, i);
+        // Проверка: если число отрицательное, больше max_ или не простое
+        if (n < 0 || n > max_ || !simple[n]) {
+            array_set(arr, i, 0);
         }
     }
     
     delete[] simple;
         
-    int writePos = 0; 
-
-    for (int i = 0; i < array_size(arr); i++) 
+    // Сдвигаем все ненулевые элементы в начало (сохраняя порядок)
+    size_t writePos = 0; 
+    for (size_t i = 0; i < size; i++) 
     {
         if (array_get(arr, i) != 0) 
         {
@@ -71,7 +104,8 @@ void task1(Array *arr, std::ofstream &output)
         }
     }
 
-    for (int i = 0; i < array_size(arr); i++)
+    // Вывод результата
+    for (size_t i = 0; i < size; i++)
     {
         output << array_get(arr, i) << " ";
         std::cout << array_get(arr, i) << " ";
@@ -89,7 +123,7 @@ void task2(Array *arr, std::ofstream& output)
         list_[i] = 0;
     }
 
-    for (int i = 0; i < array_size(arr); i++)
+    for (size_t i = 0; i < array_size(arr); i++)
     {
         list_[array_get(arr, i)]++;
     }
